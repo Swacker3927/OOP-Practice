67dceaf541ffb160c68e48c0cc44e4efe4d578dd
Taewoo-Lee <xodn3130@gmail.com>
2022-11-27 16:47:43 +0900

    사칙연산 관련 기능들 삭제

--- src/main/java/org/example/calculate/NewArithmeticOperator.java
+++
@@ -1,6 +0,0 @@
-package org.example.calculate;
-
-public interface NewArithmeticOperator {
-    boolean supports(String operator);
-    double calculate(PositiveNumber operand1, PositiveNumber operand2);
-}
